52492af3a6ed35aed85eaf40bd84f766566c2fbe
codered23 <oleg2003andriy@gmail.com>
2023-08-08 16:40:43 +0300

    commit

---
+++ src/main/java/com/example/bookstore/service/impl/BookServiceImpl.java
@@ -0,0 +1,28 @@
+package com.example.bookstore.service.impl;
+
+import com.example.bookstore.model.Book;
+import com.example.bookstore.repository.BookRepository;
+import com.example.bookstore.service.BookService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookServiceImpl implements BookService {
+    private BookRepository bookRepository;
+
+    @Autowired
+    public BookServiceImpl(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public Book save(Book product) {
+        return bookRepository.save(product);
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return bookRepository.findAll();
+    }
+}
