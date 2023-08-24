896968d66c11dccc0fc68b4487b5ee00f6b2a22e
codered23 <oleg2003andriy@gmail.com>
2023-08-24 19:17:39 +0300

    commit

--- src/main/java/com/example/bookstore/repository/BookRepository.java
+++ src/main/java/com/example/bookstore/repository/BookRepository.java
@@ -1,9 +1,13 @@
 package com.example.bookstore.repository;
 
 import com.example.bookstore.model.Book;
+import java.util.List;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
-
+    @Override
+    <S extends Book> List<S> findAll(Example<S> example, Sort sort);
 }
