765e0a49eb096c9980fee916510372740ed1cc4e
codered23 <oleg2003andriy@gmail.com>
2023-09-03 23:16:24 +0300

    task4

--- src/main/java/com/example/bookstore/dto/CreateBookRequestDto.java
+++ src/main/java/com/example/bookstore/dto/CreateBookRequestDto.java
@@ -1,12 +1,17 @@
 package com.example.bookstore.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class CreateBookRequestDto {
+    @NotNull
     private String title;
+    @NotNull
     private String author;
+    @Min(0)
     private BigDecimal price;
     private String isbn;
     private String description;
